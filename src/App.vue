059852d9196199ec0e93bf4fcea0142a7781449f
gindrianjafy <georges-antonio.indrianjafy@intercloud.com>
2021-02-08 22:50:31 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+
+  <Navbar/>
+ 
+ 
   <router-view />
+  
+  <Footer/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "../node_modules/bulma/bulma";
+</style>
 
-#nav {
-  padding: 30px;
+<script>
+import NavbarVue from './components/Navbar.vue'
+import FooterVue from './components/Footer.vue'
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+   components: {
+    'Navbar': NavbarVue,
+    'Footer': FooterVue
   }
 }
-</style>
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar is-white topNav">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="../">
+          <!-- <img src="../images/bulma.png" width="112" height="28"> -->
+        </a>
+        <div class="navbar-burger burger" data-target="topNav">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div id="topNav" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link class="navbar-item" to="/"> New </router-link>
+          <router-link class="navbar-item" to="/search"> Search</router-link>
+          <router-link class="navbar-item" to="/ask">Ask</router-link>
+          <router-link class="navbar-item" to="/show">Show</router-link>
+          <router-link class="navbar-item" to="/jobs">Jobs</router-link>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field is-grouped">
+              <p class="control">
+                <a class="button is-small">
+                  <span> Register </span>
+                </a>
+              </p>
+              <p class="control">
+                <a class="button is-small is-info is-outlined">
+                  <span>Login</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
